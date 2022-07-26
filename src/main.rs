e25bce5af8684aeaea691ee00b14061e876b57e8
Wojciech Kępka <wojciech@wkepka.dev>
2022-07-26 19:59:41 +0200

    Add initial clap

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,40 @@
-// https://swagger.io/specification/v2/#definitionsObject
-
 use swagger_rustgen::v2::{codegen::CodeGenerator, Swagger};
 
-fn main() {
-    let yaml = std::fs::read_to_string("/home/wojtek/Downloads/swagger-v4.2.yaml").unwrap();
-    let swagger: Swagger = serde_yaml::from_str(&yaml).unwrap();
+use clap::{Parser, Subcommand};
 
-    let mut codegen = CodeGenerator::new(swagger);
-    codegen.generate_models(&mut std::io::stdout()).unwrap();
+#[derive(Parser)]
+struct SwaggerRustgen {
+    #[clap(subcommand)]
+    subcommand: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Generate {
+        #[clap(subcommand)]
+        target: GenerateTarget,
+    },
+}
+
+#[derive(Subcommand)]
+enum GenerateTarget {
+    Models {
+        swagger_location: std::path::PathBuf,
+    },
+}
+
+fn main() {
+    let rustgen = SwaggerRustgen::parse();
+
+    match rustgen.subcommand {
+        Command::Generate { target } => match target {
+            GenerateTarget::Models { swagger_location } => {
+                let yaml = std::fs::read_to_string(swagger_location).unwrap();
+                let swagger: Swagger = serde_yaml::from_str(&yaml).unwrap();
+
+                let mut codegen = CodeGenerator::new(swagger);
+                codegen.generate_models(&mut std::io::stdout()).unwrap();
+            }
+        },
+    }
 }
