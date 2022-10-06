555ca8cf2f6fb66c4b81280c3caf55219876b24d
Wojciech Kępka <wojciech@wkepka.dev>
2022-10-06 12:41:46 +0200

    Update clap and add `--language` flag with a Rust default

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use swagger_rustgen::v2::{
 };
 
 use clap::{Parser, Subcommand};
+use std::fmt;
 
 #[derive(Parser)]
 struct SwaggerRustgen {
@@ -25,8 +26,29 @@ enum Command {
 #[derive(Subcommand)]
 enum GenerateTarget {
     Models {
+        #[arg(short, long, default_value_t = Language::Rust)]
+        language: Language,
         swagger_location: std::path::PathBuf,
     },
+}
+
+#[derive(clap::ValueEnum, Clone)]
+enum Language {
+    Rust,
+}
+
+impl AsRef<str> for Language {
+    fn as_ref(&self) -> &str {
+        match self {
+            Language::Rust => "rust",
+        }
+    }
+}
+
+impl fmt::Display for Language {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.as_ref())
+    }
 }
 
 fn main() {
@@ -35,12 +57,19 @@ fn main() {
 
     match rustgen.subcommand {
         Command::Generate { target } => match target {
-            GenerateTarget::Models { swagger_location } => {
+            GenerateTarget::Models {
+                swagger_location,
+                language,
+            } => {
                 let yaml = std::fs::read_to_string(swagger_location).unwrap();
-                let swagger: Swagger<RustType> = serde_yaml::from_str(&yaml).unwrap();
 
-                let backend = Box::new(RustCodegen::default());
-                let mut codegen = CodeGenerator::new(swagger, backend);
+                let mut codegen = match language {
+                    Language::Rust => {
+                        let swagger: Swagger<RustType> = serde_yaml::from_str(&yaml).unwrap();
+                        let backend = Box::new(RustCodegen::default());
+                        CodeGenerator::new(swagger, backend)
+                    }
+                };
                 let mut writer = Box::new(std::io::stdout()) as Box<dyn std::io::Write>;
                 codegen.generate_models(&mut writer).unwrap();
             }
